9c110bdf3bb4ae7b0c9c295b17e9866dbfe5846f
LolaDP <72499343+LolaDP@users.noreply.github.com>
2021-11-22 23:16:29 +0100

    v2

--- Map.java
+++ Map.java
@@ -12,49 +12,20 @@ public class Map {
 		this.split = s;
 	}
 	
-	public ArrayList<HashMap<String, Integer>> map(){
+	public HashMap<String, Integer> map(){
 		//method which reads the file and returns a HashMap<word,key>
-		ArrayList<HashMap<String, Integer>> element=new ArrayList<HashMap<String, Integer>>();
+		HashMap<String, Integer> element=new HashMap<String, Integer>();
 		for(int i = 0; i < split.size(); i++){
 			//list of word
 			String[] words = split.get(i).split(" ");
 			for (int j = 0; j < words.length; j++) {
-				HashMap<String, Integer> word=new HashMap<String, Integer>();
-				word.put(words[j],1);
-				element.add(word);
+				element.put(words[j],1);
 			}
 		}
 		return element;
 	}
 	
-	/*public ArrayList<HashMap<String, Integer>> map(){
-		//method which reads the file and returns a HashMap<word,key>
-		ArrayList<HashMap<String, Integer>> element=new ArrayList<HashMap<String, Integer>>();
-		try {
-			File file=new File(fName);
-			// Creating the File Reader object
-			FileReader fr=new FileReader(file);
-			// Creating the BufferedReader object 
-			BufferedReader br = new BufferedReader(fr); 
-			String line= null;
-			//the loop will read line by line
-			while((line = br.readLine())!= null){
-				//list of word
-				String[] words = line.split(" ");
-				for (int i = 0; i < words.length; i++) {
-					HashMap<String, Integer> word=new HashMap<String, Integer>();
-					word.put(words[i],1);
-					element.add(word);
-				}
-			}
-			fr.close();  
-		}
-		catch(IOException e){
-			System.out.println("An error occurred.");
-		    e.printStackTrace();
-	    }
-		return element;
-	}*/
+
 	}
 
 

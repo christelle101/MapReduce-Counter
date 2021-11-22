2c7d2bcb236ac14eeb755e1ff43bb8cb9cfea533
LolaDP <72499343+LolaDP@users.noreply.github.com>
2021-11-22 23:34:41 +0100

    to be modified

--- Reduce.java
+++ Reduce.java
@@ -4,20 +4,27 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Reduce {
-	public ArrayList<HashMap<String, Integer>> split =new ArrayList<HashMap<String, Integer>>();
-	//0 correspond a la partie des mots de a-m et 1 de n-z
-	public int part_number;
+	public ArrayList<ArrayList<HashMap<String, Integer>>> split =new ArrayList<ArrayList<HashMap<String, Integer>>>();
+	/*//0 correspond a la partie des mots de a-m et 1 de n-z
+	public int part_number;*/
 
 	public /*ArrayList<HashMap<String, Integer>>*/ void reduce() {
 		ArrayList<HashMap<String, Integer>> map = new ArrayList<HashMap<String, Integer>>();
 		for (int i = 0; i< split.size(); i++) {
-			for (String j : split.get(i).keySet()) {
-				if (part_number==0 & j.substring(0,1).matches("[a-mA-M]*")) {
+			for (int  j= 0; j< split.get(i).size(); i++) {
+				for (String k : split.get(i).get(j).keySet()) {
+					/*if (part_number==0 & k.substring(0,1).matches("[a-mA-M]*")) {
 					
-				}
+					}*/
+					for (int l = 0; l<map.size(); l++) {
+						if (k.equals(map.get(l).keySet())) {
+							//map.get(l).;
+								} 
 				
+						
+					}
+				}
 			}
 		}
 	}
-
 }

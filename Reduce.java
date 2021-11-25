835597f8235a2aeb26444e8ab62f32d5742520fe
LolaDP <72499343+LolaDP@users.noreply.github.com>
2021-11-25 20:42:06 +0100

    Update Reduce.java

--- Reduce.java
+++ Reduce.java
@@ -1,14 +1,15 @@
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Reduce {
-	public ArrayList<ArrayList<HashMap<String, Integer>>> split =new ArrayList<ArrayList<HashMap<String, Integer>>>();
+	public ArrayList<ArrayList<HashMap<String, Integer>>> list =new ArrayList<ArrayList<HashMap<String, Integer>>>();
 	/*//0 correspond a la partie des mots de a-m et 1 de n-z
 	public int part_number;*/
 	
 	public Reduce(ArrayList<ArrayList<HashMap<String, Integer>>> split) {
 		super();
-		this.split = split;
+		this.list = split;
 	}
 
 	public ArrayList<HashMap<String, Integer>> reduce() {
@@ -17,12 +18,12 @@ public class Reduce {
 		//liste stockant les cles sous format string
 		ArrayList<String> words = new ArrayList<String>();
 		
-		for (int i = 0; i< split.size(); i++) {
-			for (int  j= 0; j< split.get(i).size(); j++) {
+		for (int i = 0; i< list.size(); i++) {
+			for (int  j= 0; j< list.get(i).size(); j++) {
 					//si le mot cle n'a pas deja ete rencontre on le rajoute dans words er son hashmap dans map
-					if (!words.contains(split.get(i).get(j).keySet().toString())){
-						words.add(split.get(i).get(j).keySet().toString());
-						map.add(split.get(i).get(j));
+					if (!words.contains(list.get(i).get(j).keySet().toString())){
+						words.add(list.get(i).get(j).keySet().toString());
+						map.add(list.get(i).get(j));
 						//System.out.println("map "+map);
 						//System.out.println(words.contains(split.get(i).get(j).keySet()));
 					}
@@ -31,7 +32,7 @@ public class Reduce {
 					//et on on incremente le compteur d'occurneve
 					else {
 						for (int l = 0; l<=map.size()-1; l++) {
-							String keySplit=(String) split.get(i).get(j).keySet().toArray()[0];
+							String keySplit=(String) list.get(i).get(j).keySet().toArray()[0];
 							String keyMap=(String) map.get(l).keySet().toArray()[0];
 							
 							//System.out.println("l "+l +" "+ map.get(l));
